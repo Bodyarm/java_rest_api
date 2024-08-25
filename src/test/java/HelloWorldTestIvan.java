04b445dde584b28a834f8f766e57e25101d17c15
Bodyarm <i.v.gen@yandex.ru>
2024-08-25 16:32:42 +0200

    Ex3

---
+++ src/test/java/HelloWorldTestIvan.java
@@ -0,0 +1,10 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTestIvan {
+    @Test
+    public void testHelloWorld(){
+        System.out.println("Hello from Ivan");
+    }
+}
