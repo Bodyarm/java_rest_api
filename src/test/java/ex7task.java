8d68577419e5f847f9c268d8a904cf8eb715dcd4
Bodyarm <i.v.gen@yandex.ru>
2024-09-19 00:43:51 +0200

    Ex7

---
+++ src/test/java/ex7task.java
@@ -0,0 +1,40 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+public class ex7task {
+
+    @Test
+    public void ex7task(){
+
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        int responseStatus = response.getStatusCode();
+        String responseLocation = response.getHeaders().getValue("Location");
+
+        while(responseStatus!=200){
+            response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(responseLocation)
+                    .andReturn();
+            responseStatus=response.getStatusCode();
+            //Если доходим до 200тки, то заголовка Location Не будет и мы получим null
+            //Поэтому в переменной оставлем URL на который мы уже пришли
+            if(responseStatus!=200) responseLocation = response.getHeaders().getValue("Location");
+        }
+
+        System.out.println(responseLocation);
+
+
+    }
+}
