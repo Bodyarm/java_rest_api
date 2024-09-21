3322df844a0668a1cc684bd1c60d79707c7e544b
Bodyarm <i.v.gen@yandex.ru>
2024-09-21 02:28:13 +0200

    Ex10 v1.1

--- src/test/java/ex10task.java
+++ src/test/java/ex10task.java
@@ -7,10 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ex10task {
 
     @ParameterizedTest
-    @ValueSource(strings = {"Less15","Exactlyyyyyyy15","Mooooooooooooooooorethan15",""})
+    @ValueSource(strings = {"Less15","Exactlyyyyyyy15","Mooooooooooooooooorethan15","nulltest"})
     public void ex10taskTest(String name) {
 
-        assertTrue(name.length()>15,"Current String '"+ name + "' is below 15 symbols");
-
+        if( name.equals("nulltest")) name =null;
+        try {
+            assertTrue(name.length() > 15, "Current String '" + name + "' is below 15 symbols");
+        } catch (NullPointerException e){
+            assertTrue(false,"Current String is null");
+        }
     }
 }
